eec1773e7c2270372d4fe95d099a1eae8b2b35b4
DonevskiPetar <donevskipetar@gmail.com>
2020-05-27 11:40:42 +0200

    .

--- CountriesApp/CountriesAppWEB/Controllers/HomeController.cs
+++ CountriesApp/CountriesAppWEB/Controllers/HomeController.cs
@@ -6,21 +6,33 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CountriesAppWEB.Models;
+using CountriesAppWEB.Repository.IRepository;
+using CountriesAppWEB.Models.ViewModels;
 
 namespace CountriesAppWEB.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ICountryRepository _countryRepository;
+        private readonly ICityRepository _cityRepository;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ICountryRepository countryRepository, ICityRepository cityRepository)
         {
+            _countryRepository = countryRepository;
+            _cityRepository = cityRepository;
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            IndexViewModel indexViewModel = new IndexViewModel()
+            {
+                CountryList = await _countryRepository.GetAllAsync(SD.CountriesAPIPath),
+                CityList = await _cityRepository.GetAllAsync(SD.CitiesAPIPath)
+            };
+
+            return View(indexViewModel);
         }
 
 
---
+++ CountriesApp/CountriesAppWEB/Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountriesAppWEB.Models.ViewModels
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Country> CountryList { get; set; }
+        public IEnumerable<City> CityList { get; set; }
+
+    }
+}
